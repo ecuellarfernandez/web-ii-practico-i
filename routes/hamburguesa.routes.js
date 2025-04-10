43791fe63970086de98d3c059f751b69e5474f6f
ecuellarfernandez <ecuellarfernandez@gmail.com>
2025-04-10 02:06:28 -0400

    add delete and detail routes for hamburguesa

--- routes/hamburguesa.routes.js
+++ routes/hamburguesa.routes.js
@@ -1,11 +1,15 @@
 module.exports = app=>{
     let router = require('express').Router();
     const controller = require("../controllers/hamburguesa.controller");
-    router.post('/:id/calificar', controller.postCalificacion);
     router.get('/page', controller.listPage);
-
     router.get('/create', controller.getForm);
     router.post('/create', controller.postCreate);
+
+    router.post('/delete/:id',controller.deleteHamburguesa);
+
+    router.get('/:id', controller.getDetail);
+    router.post('/:id/calificar', controller.postCalificacion);
+
 
     router.get('/:id/edit', controller.getEdit);
     router.post('/:id/edit', controller.postEdit);
